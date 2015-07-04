c660344e1cf1ad4b00097059612b5ba421300bda
tristanchanson <tristanchanson@gmail.com>
2015-07-04 14:22:33 -0500

    Initial Commit

---
+++ src/main/java/com/tristanchanson/Application.java
@@ -0,0 +1,48 @@
+package com.tristanchanson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@RestController
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello Spring Boot";
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatcherServletRegistration() {
+
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                dispatcherServlet(), "/*");
+
+        registration
+                .setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+
+        return registration;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
